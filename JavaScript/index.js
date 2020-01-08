1971d3aa2c89ca63924b0b3f6dc2729003c4b37a
chani3653 <tidnxm1234@gmail.com>
2020-01-08 18:37:45 +0900

    2020.01.08

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,1 +1,93 @@
-console.log('Im working. Im JS. Im beautiful. Im worth it.');
+/*
+const nicoInfo = {
+    name : "nico",
+    age :33,
+    gender:"male",
+    isHandsome: true
+}
+console.log(nicoInfo, console)
+*/
+
+/*
+2-1
+function sayHello(name, age)
+{
+    return (`Hello ${name} You are ${age} years old`);
+}
+
+const greetNicolas = sayHello("Nicolas", 14)
+console.log(greetNicolas)
+//sayHello("Nicolas", 15);
+*/
+
+/*
+//2-2
+const calculator =
+{
+    plus:function(a, b)
+    {
+        return a + b;
+    }
+}
+//console.log(greetNicolas)
+const plus = calculator.plus(5, 5)
+console.log(plus)
+*/
+/*
+//2-3
+const title =document.querySelector("#title");
+
+document.title = "I own you now "
+title.style.color = "red"
+title.innerHTML = "Hi! From JS";
+*/
+/*
+//2-4
+const title =document.querySelector("#title");
+function handleClick()
+{
+    title.style.color = "red";
+}
+
+title.addEventListener("click",handleClick);
+*/
+/*
+//2-5
+if("10" === 10)
+{
+    console.log("hi");
+}
+else if ( 10 === 10)
+{
+    console.log("lalala");
+}
+else 
+{
+    console.log("ho");
+}
+
+if ( 20 > 5 && "nicolas" === "nicolas")
+{
+    console.log("yes");
+}
+else
+{
+    console.log("no");
+}
+
+const age = prompt (" How Old are you?");
+if( age >= 18 && age <=21)
+console.log( " you can drink but you should not");
+else if (age > 21)
+console.log("you can drink")
+else
+console.log(" too young ");
+*/
+
+const title =document.querySelector("#title");
+function handleClick()
+{
+    title.style.color = "red";
+}
+
+title.addEventListener("click",handleClick);
