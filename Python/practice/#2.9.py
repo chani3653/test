d3b7ac821d29e8f5b334b83cc5124aed8c0826b5
chani3653 <tidnxm1234@gmail.com>
2020-01-20 19:46:54 +0900

    2020.01.20

--- Python/practice/#2.9.py
+++ Python/practice/#2.9.py
@@ -1,3 +1,14 @@
+#main
+"""
+from indeed import get_jobs as get_indeed_jobs
+from so import get_jobs as get_so_jobs
+
+#indeed_jobs = get_indeed_jobs()
+so_jobs = get_so_jobs()
+"""
+
+#indeed
+"""
 import requests
 from bs4 import BeautifulSoup
 
@@ -47,3 +58,23 @@ def get_jobs():
   last_page = get_last_page()
   jobs = extract_jobs(last_page)
   return jobs
+"""
+
+#so
+"""
+import requests
+from bs4 import BeautifulSoup
+
+
+URL = f"https://stackoverflow.com/jobs?q=python&sort=i"
+
+def get_last_page():
+  result = requests.get(URL)
+  soup = BeautifulSoup(result.text,"html.parser")
+  pages = soup.find("div", {"class":"s-pagination"}).find_all("a")
+  print(pages)
+
+def get_jobs():
+  last_page = get_last_page()
+  return []
+  """
