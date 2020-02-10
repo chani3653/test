a64af09dc34db377f6db21e7c7b1ed0510a67e70
chani3653 <tidnxm1234@gmail.com>
2020-02-10 17:14:24 +0900

    2020.02.10

--- Python/Django/rooms/models.py
+++ Python/Django/rooms/models.py
@@ -68,10 +68,10 @@ class Room(core_models.TimeStampedModel):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default = False)
-    host = models.ForeignKey("users.User", on_delete = models.CASCADE)
+    host = models.ForeignKey("users.User", related_name = "rooms", on_delete = models.CASCADE)
     room_type = models.ForeignKey("RoomType", on_delete = models.SET_NULL, null = True)
     amenities = models.ManyToManyField("Amenity", blank = True)
-    favilities = models.ManyToManyField("Facility", blank = True)
+    facilities = models.ManyToManyField("Facility", blank = True)
     house_rules = models.ManyToManyField("HouseRule", blank = True)
     
     def __str__(self):
